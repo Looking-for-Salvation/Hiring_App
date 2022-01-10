dc990e06831445098e90eb5051a4cd34fdb7750d
Death <M.26A20.M@Gmail.com>
2022-01-10 15:30:56 +0330

    Installed and configured Vue-Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import router from "./router.js";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/router.js
@@ -0,0 +1,6 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [],
+});
