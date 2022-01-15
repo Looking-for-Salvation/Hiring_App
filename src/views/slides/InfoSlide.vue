e5665dd661fa37bff4dc5b3aaf9d66451f25f165
Death <M.26A20.M@Gmail.com>
2022-01-15 17:55:53 +0330

    minor fixes

--- src/views/slides/InfoSlide.vue
+++ src/views/slides/InfoSlide.vue
@@ -13,11 +13,11 @@
 							<div class="flex flex-col items-center mb-4 sm:flex-row">
 								<input
 									v-model="formData.phone"
-									type="text"
+									type="number"
 									placeholder="شماره تلفن"
 									class="w-full mb-4 sm:mb-0 sm:ml-4 form-control input input-bordered"
 								/>
-								<input v-model="formData.email" type="text" placeholder="ایمیل" class="w-full input input-bordered" />
+								<input v-model="formData.email" type="email" placeholder="ایمیل" class="w-full input input-bordered" />
 							</div>
 							<div class="flex flex-col justify-center w-full mb-4 sm:items-center sm:flex-row">
 								<select v-model="formData.grade" class="mb-4 sm:flex-1 sm:mb-0 sm:ml-auto select select-bordered">
@@ -27,7 +27,7 @@
 									<option value="karshenasi">کارشناسی</option>
 								</select>
 								<input v-model="formData.field" type="text" placeholder="رشته تحصیلی" class="mb-4 input-bordered sm:flex-1 sm:mb-0 sm:mx-4 input" />
-								<input type="text" placeholder="تاریخ تولد" class="sm:flex-1 sm:mr-auto input input-bordered date-picker" />
+								<input type="date" placeholder="تاریخ تولد" class="sm:flex-1 sm:mr-auto input input-bordered date-picker" />
 								<date-picker v-model="formData.birthDate" custom-input=".date-picker"></date-picker>
 							</div>
 							<textarea v-model="formData.address" class="w-full mb-8 textarea textarea-bordered min-h-[100px]" placeholder="آدرس"></textarea>
