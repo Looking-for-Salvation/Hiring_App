1c1e35554bdcccb59eb34a315667099603320a2f
Death <M.26A20.M@Gmail.com>
2022-01-17 15:33:33 +0330

    Added BaseQuestion component

---
+++ src/components/ui/BaseQuestion.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="flex-1 w-full md:px-10">
+		<div class="flex items-center h-full md:px-10">
+			<div class="container w-full mx-auto">
+				<div class="max-w-xl mx-auto">
+					<h1 class="mb-12 text-3xl font-light tracking-tight text-center text-black">سوال اول</h1>
+					<h3 class="mb-8 text-xl">خودتان را در یک کلمه چطور توصیف می کنید؟</h3>
+					<div class="grid grid-cols-3">
+						<input type="text" class="col-start-1 col-end-3 input input-bordered" />
+						<h6 class="mr-8 text-base text-black">راهنما:</h6>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		questionData: {
+			type: Object,
+			required: true,
+		},
+	},
+	setup() {},
+};
+</script>
