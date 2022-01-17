acbe404ee0516b35079cb9785640cb9f2701898d
Death <M.26A20.M@Gmail.com>
2022-01-17 23:30:42 +0330

    Removed several files, minor changes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "@/views/HomePage.vue";
-import HiringPage from "@/views/HiringPage.vue";
-import IntroPage from "@/views/IntroPage.vue";
 
 const router = createRouter({
 	history: createWebHistory(),
-	routes: [
-		{ name: "Intro", path: "/", component: IntroPage },
-		{ name: "Home", path: "/home", component: HomePage },
-		{ name: "Hiring", path: "/hiring", component: HiringPage },
-	],
+	routes: [{ name: "Home", path: "/", component: HomePage }],
 });
 
 export default router;
--- src/views/HiringPage.vue
+++
@@ -1,4 +0,0 @@
-<template>
-	<div>Hiring Page</div>
-	<router-link :to="{ name: 'Home' }">Take me to Home page!</router-link>
-</template>
